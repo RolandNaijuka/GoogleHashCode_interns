6880a41cd3b5d69afe13e5269e6f56d991baa3c6
Roland Naijuka <rnaijuka@google.com>
2020-08-03 19:24:38 -0400

    read data
    Signed-off-by: Roland Naijuka <rnaijuka@google.com>

--- PercSolution/src/com/perc/Main.java
+++ PercSolution/src/com/perc/Main.java
@@ -1,8 +1,22 @@
 package com.perc;
 
+import java.io.*;
+import java.util.*;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    try {
+      File file = new File("../data/example.in");
+      Scanner scanner = new Scanner(file);
+      while (scanner.hasNextLine()) {
+        String data = scanner.nextLine();
+        System.out.println(data);
+        break;
+      }
+    } catch (Exception e) {
+      
+    } finally {
+    
+    }
   }
 }
