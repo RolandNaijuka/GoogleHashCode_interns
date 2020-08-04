2aa68149ce1b363214e47f839053cce770b2e580
CharlesJ10 <chiderajnr@gmail.com>
2020-08-03 19:24:29 -0500

    Video Structure

--- PercSolution/src/com/perc/classes/Video.java
+++ PercSolution/src/com/perc/classes/Video.java
@@ -1,7 +1,24 @@
 package com.perc.classes;
 
 public class Video {
-  // ID
-  // Sizes
-  // 
+
+  private String videoId;
+  private int videoSize;
+
+  public int getVideoSize() {
+    return videoSize;
+  }
+
+  public void setVideoSize(int videoSize) {
+    this.videoSize = videoSize;
+  }
+
+  public String getVideoId() {
+    return videoId;
+  }
+
+  public void setVideoId(String videoId) {
+    this.videoId = videoId;
+  }
+
 }
