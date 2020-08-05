e3a57e97f26483e916668b384b5a62bae17d134b
RolandNaijuka <rnaijuka@u.rochester.edu>
2020-08-05 03:44:00 -0400

    Revised the algorithm to add the videos according to the number of requests

--- PercSolution/src/com/perc/Main.java
+++ PercSolution/src/com/perc/Main.java
@@ -4,19 +4,20 @@ import com.perc.classes.CacheServer;
 import com.perc.classes.DataCenter;
 import com.perc.classes.EndPoint;
 import com.perc.classes.Video;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
+  private static final String SEPARATOR = "#";
 
   public static void main(String[] args) {
     try {
 //      use the pathname as "data/example.in" while running on terminal
 //      use the pathname as "src/com/perc/data/example.in" while running in IntelliJ IDEA or eclipse
-      String path = new File("src/com/perc/data/vloggers_of_the_world.in").getAbsolutePath();
+      String path = new File("src/com/perc/data/example.in").getAbsolutePath();
       File file = new File(path);
       Scanner scanner = new Scanner(file);
       int numVideo = scanner.nextInt();
@@ -25,7 +26,7 @@ public class Main {
       int numCacheServers = scanner.nextInt();
       int sizeCacheServer = scanner.nextInt();
 //      Create all cache servers
-      ArrayList<CacheServer>  cacheServers =  new ArrayList<>();
+      ArrayList<CacheServer> cacheServers = new ArrayList<>();
       for (int cacheServerId = 0; cacheServerId < numCacheServers; cacheServerId++) {
         cacheServers.add(new CacheServer(cacheServerId, sizeCacheServer));
       }
@@ -41,7 +42,7 @@ public class Main {
         int dataCenterLatency = scanner.nextInt();
         int numOfConnectedCaches = scanner.nextInt();
         EndPoint endPoint = new EndPoint(endPointId, dataCenterLatency);
-        while(numOfConnectedCaches > 0) {
+        while (numOfConnectedCaches > 0) {
           int cacheServerId = scanner.nextInt();
           int latency = scanner.nextInt();
           endPoint.addCacheLatency(cacheServerId, latency);
@@ -50,20 +51,27 @@ public class Main {
         endPoints.add(endPoint);
       }
 //      Add all the requests
+      HashMap<String, Integer> videoRequests = new HashMap<>();
       int tempNumRequests = numRequests;
-      while (tempNumRequests > 0)  {
+      while (tempNumRequests > 0) {
         int videoId = scanner.nextInt();
         int endPointId = scanner.nextInt();
         int num_of_requests = scanner.nextInt();
+
+//        Add video requests to a map which is gonna be sorted later
+        String videoId_endPointId = String.format("%d%s%d", videoId, SEPARATOR, endPointId);
+        videoRequests.put(videoId_endPointId, num_of_requests);
+
         endPoints.get(endPointId).addVideoRequest(videoId, num_of_requests);
-        tempNumRequests --;
+        tempNumRequests--;
       }
 
       scanner.close();
 
 
       DataCenter dataCenter = new DataCenter();
-      minimizeLatency(endPoints, cacheServers, videos, dataCenter);
+//      minimizeLatency(endPoints, cacheServers, videos, dataCenter);
+      minimizeLatencies(videoRequests, endPoints, cacheServers, videos, dataCenter);
 
       List<CacheServer> filteredCacheServers = cacheServers
               .stream()
@@ -71,56 +79,64 @@ public class Main {
               .collect(Collectors.toList());
       String pathOut = new File("src/com/perc/data/vloggers_of_the_world.out").getAbsolutePath();
       File output = new File(pathOut);
-      output.createNewFile();
 
       FileWriter fileWriter = new FileWriter(output);
-      fileWriter.write(filteredCacheServers.size()+"\n");
+      fileWriter.write(filteredCacheServers.size() + "\n");
 
-      for (CacheServer cacheServer: filteredCacheServers) {
-        fileWriter.write(cacheServer.toString()+"\n");
+      for (CacheServer cacheServer : filteredCacheServers) {
+        fileWriter.write(cacheServer.toString() + "\n");
       }
 
       fileWriter.close();
 
     } catch (Exception e) {
-      System.out.println(e);
+      System.err.println(e.toString());
     }
   }
 
-// Boiler plate for the algorithm
-  public static void minimizeLatency(ArrayList<EndPoint> endPoints, ArrayList<CacheServer>  cacheServers, ArrayList<Video> videos, DataCenter dataCenter) {
-    for (EndPoint endPoint: endPoints) {
-      HashMap<Integer, Integer> videoRequestsUnsorted = endPoint.videoRequests;
-      HashMap<Integer, Integer> cacheLatenciesUnsorted = endPoint.cacheLatency;
+  private static HashMap<String, Integer> sortMapDesc(@NotNull HashMap<String, Integer> videoRequestsUnSorted) {
+    HashMap<String, Integer> videoRequestsSorted = new HashMap<>();
+    videoRequestsUnSorted.entrySet()
+            .stream()
+            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+            .forEachOrdered(x -> videoRequestsSorted.put(x.getKey(), x.getValue()));
+    return videoRequestsSorted;
+  }
 
-//      Sorted in descending order of video requests
-      HashMap<Integer, Integer> videoRequests = new HashMap<>();
-      videoRequestsUnsorted.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-              .forEachOrdered(x -> videoRequests.put(x.getKey(), x.getValue()));
-//      Sorted in ascending order of latencies
+  private static void minimizeLatencies(
+          @NotNull HashMap<String, Integer> videoRequestsUnSorted,
+          @NotNull ArrayList<EndPoint> endPoints,
+          @NotNull ArrayList<CacheServer>  cacheServers,
+          @NotNull ArrayList<Video> videos,
+          @NotNull DataCenter dataCenter
+  ) {
+    HashMap<String, Integer> videoRequestsSorted = sortMapDesc(videoRequestsUnSorted);
+    for(String key: videoRequestsSorted.keySet()) {
+      String[] ids = key.split(SEPARATOR);
+      int videoId = Integer.parseInt(ids[0]);
+      int endPointId = Integer.parseInt(ids[1]);
+
+      HashMap<Integer, Integer> cacheLatenciesUnsorted = endPoints.get(endPointId).cacheLatency;
+      //      Sorted in ascending order of latencies
       HashMap<Integer, Integer> cacheLatencies = new HashMap<>();
       cacheLatenciesUnsorted.entrySet()
               .stream()
               .sorted(Map.Entry.comparingByValue())
               .forEachOrdered(x -> cacheLatencies.put(x.getKey(), x.getValue()));
 
-
-      for(int videoId: videoRequests.keySet()) {
-        int num_of_video_requests = videoRequests.get(videoId);
-        int videoSize = videos.get(videoId).videoSize;
-        boolean videoAdded = false;
-        for (int cacheId: cacheLatencies.keySet()) {
-          CacheServer cacheServer = cacheServers.get(cacheId);
-          if (videoSize <= cacheServer.maxStorage && !cacheServer.containsVideo(videoId)) {
-            cacheServer.maxStorage -= videoSize;
-            cacheServer.addVideo(videoId);
-            videoAdded = true;
-            break;
-          }
+      int videoSize = videos.get(videoId).videoSize;
+      boolean videoAdded = false;
+      for (int cacheId: cacheLatencies.keySet()) {
+        CacheServer cacheServer = cacheServers.get(cacheId);
+        if (videoSize <= cacheServer.maxStorage && !cacheServer.containsVideo(videoId)) {
+          cacheServer.maxStorage -= videoSize;
+          cacheServer.addVideo(videoId);
+          videoAdded = true;
+          break;
         }
-        if (!videoAdded) {
-          dataCenter.addVideo(videoId);
-        }
+      }
+      if (!videoAdded) {
+        dataCenter.addVideo(videoId);
       }
     }
   }
